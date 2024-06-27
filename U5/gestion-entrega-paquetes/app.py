fc5f742e087c3d939f0e70dc8558dfc784bc4e47
German Gambarte <germangambarte@gmail.com>
2024-06-27 09:42:31 -0300

    add all features

--- U5/gestion-entrega-paquetes/app.py
+++ U5/gestion-entrega-paquetes/app.py
@@ -1,78 +1,137 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 
-from models import db
+from flask import Flask, flash, redirect, render_template, request, session
+
+from models import Paquete, Sucursal, Transporte, db
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
 db.init_app(app)
 
-# sucursal_1 = Sucursal(numero=1,provincia="Buenos Aires", localidad="La Plata")
-# sucursal_1 = Sucursal.c
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def inicio():
-    return render_template("inicio.html")
+    session["sucursal"] = ""
+    sucursales = Sucursal.query.order_by(Sucursal.numero).all()
+    return render_template("inicio.html", sucursales=sucursales)
 
 
 @app.route("/despachante", methods=["GET"])
-def obtener_datos_despachante():
-    return render_template("despachante-inicio.html")
+def menu_despachante():
+    session["sucursal"] = request.args.get("sucursal")
+    return render_template("menu-despachante.html")
 
 
-@app.route("/despachante/seleccionar-sucursal", methods=["GET"])
-def seleccionar_sucursal():
-    sucursales = [
-        {
-            "codigo": "01",
-            "provincia": "San Juan",
-            "localidad": "Capital",
-            "direccion": "direccion",
-        },
-        {
-            "codigo": "02",
-            "provincia": "San Juan",
-            "localidad": "Sta Lucia",
-            "direccion": "direccion",
-        },
-        {
-            "codigo": "03",
-            "provincia": "Mendoza",
-            "localidad": "Capital",
-            "direccion": "direccion",
-        },
-        {
-            "codigo": "04",
-            "provincia": "Mendoza",
-            "localidad": "Tupungato",
-            "direccion": "direccion",
-        },
-        {
-            "codigo": "05",
-            "provincia": "Cordoba",
-            "localidad": "Capital",
-            "direccion": "direccion",
-        },
-    ]
-    return render_template("seleccionar-sucursal.html", sucursales=sucursales)
-
-
-@app.route("/despachante/<int:sucursal>/registrar-paquete", methods=["GET", "POST"])
+@app.route("/despachante/registro-paquete", methods=["GET", "POST"])
 def registrar_paquete():
-    sucursal = request.form.get("sucursal")
     if request.method == "POST":
         if (
-            request.form["destino"]
-            and request.form["destinatario"]
-            and request.form["peso"],
+            request.form.get("destino") == ""
+            or request.form.get("destinatario") == ""
+            or request.form.get("peso") == ""
         ):
-            print("Todo bien")
+            flash("Todos los campos son obligatorios", "error")
+            return redirect("/despachante/registro-paquete")
 
-        return render_template("registrar-paquete.html")
+        if session.get("sucursal") == "":
+            flash("No seleccionó la sucursal en la que opera", "error")
+            return redirect("/")
+
+        numero_envio = 20
+
+        ultimo_paquete = Paquete.query.order_by(Paquete.numeroenvio.desc()).first()
+        if ultimo_paquete is not None:
+            numero_envio = ultimo_paquete.numeroenvio + 20
+
+        nuevo_paquete = Paquete(
+            numeroenvio=numero_envio,
+            peso=int(request.form.get("peso")),
+            nomdestinatario=request.form.get("destinatario"),
+            dirdestinatario=request.form.get("destino"),
+            entregado=False,
+            idrepartidor=0,
+            idsucursal=session.get("sucursal"),
+        )
+        db.session.add(nuevo_paquete)
+        db.session.commit()
+        flash("El paquete se registró exitosamente", "success")
+        return redirect("/despachante")
+
     else:
-        return render_template("registrar-paquete.html", sucursal=sucursal)
+        return render_template("registro-paquete.html")
+
+
+@app.route("/despachante/registro-salida", methods=["GET", "POST"])
+def registro_salida():
+    if request.method == "POST":
+        if request.form.get("sucursal") == "" or request.form.getlist("paquete") == []:
+            flash("Debe seleccionar al menos un paquete y una sucursal", "error")
+            return redirect("/despachante/registro-salida")
+        id_sucursal = request.form.get("sucursal")
+        paquetes = request.form.getlist("paquete")
+
+        nuevo_numero_transporte = 1
+
+        ultimo_transporte = Transporte.query.order_by(
+            Transporte.numerotransporte.desc()
+        ).first()
+
+        if ultimo_transporte is not None:
+            nuevo_numero_transporte = ultimo_transporte.numerotransporte + 1
+
+        nuevo_transporte = Transporte(
+            numerotransporte=nuevo_numero_transporte,
+            fechahorasalida=datetime.now(),
+            fechahorallegada=datetime.now(),
+            idsucursal=id_sucursal,
+        )
+
+        db.session.add(nuevo_transporte)
+        db.session.commit()
+
+        for id in paquetes:
+            paquete = Paquete.query.filter_by(id=id).first()
+            if paquete:
+                paquete.idtransporte = nuevo_transporte.id
+                db.session.commit()
+
+        flash("La salida del transporte se registró exitosamente", "success")
+        return redirect("/despachante")
+
+    else:
+        sucursales = Sucursal.query.order_by(Sucursal.numero).all()
+        paquetes = Paquete.query.filter_by(entregado=False, idrepartidor=0).all()
+        return render_template(
+            "registro-salida.html", sucursales=sucursales, paquetes=paquetes
+        )
+
+
+@app.route("/despachante/registro-llegada", methods=["GET", "POST"])
+def registro_llegada():
+    if request.method == "POST":
+        id_transporte = request.form.get("transporte")
+        transporte = Transporte.query.get(id_transporte)
+
+        if transporte is None:
+            flash("Debe seleccionar un transporte", "error")
+            return redirect("/despachante/registro-llegada")
+
+        transporte.fechahorallegada = datetime.now()
+
+        db.session.commit()
+
+        flash("La llegada del transporte se registró exitosamente", "success")
+        return redirect("/despachante")
+    else:
+        transportes = Transporte.query.filter_by(
+            fechahorallegada=None, idsucursal=session.get("sucursal")
+        ).all()
+
+        return render_template("registro-llegada.html", transportes=transportes)
 
 
 if __name__ == "__main__":
--- U5/gestion-entrega-paquetes/models.py
+++ U5/gestion-entrega-paquetes/models.py
@@ -1,47 +1,71 @@
-from __main__ import app
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy(app)
-
-
-class Paquete(db.Model):
-    __tablename__ = "paquete"
-    id = db.Column(db.Integer, primary_key=True)
-    numero_envio = db.Column(db.Integer)
-    peso = db.Column(db.Integer)
-    destinatario = db.Column(db.Text)
-    entregado = db.Column(db.Boolean)
-    observaciones = db.Column(db.Text)
-    id_sucursal = db.Column(db.Integer, db.ForeignKey("sucursal.id"))
-    id_transporte = db.Column(db.Integer, db.ForeignKey("transporte.id"))
-    id_repartidor = db.Column(db.Integer, db.ForeignKey("repartidor.id"))
-
-
-class Repartidor(db.Model):
-    __tablename__ = "repartidor"
-    id = db.Column(db.Integer, primary_key=True)
-    numero = db.Column(db.Integer)
-    nombre = db.Column(db.Text)
-    dni = db.Column(db.Text)
-    id_sucursal = db.Column(db.Integer, db.ForeignKey("sucursal.id"))
-    comentario = db.relationship(
-        "Comentario", backref="usuario", cascade="all, delete-orphan"
-    )
+db = SQLAlchemy()
 
 
 class Sucursal(db.Model):
     __tablename__ = "sucursal"
     id = db.Column(db.Integer, primary_key=True)
-    numero = db.Column(db.Integer)
-    provincia = db.Column(db.Text)
-    localidad = db.Column(db.Text)
-    direccion = db.Column(db.Text)
+    numero = db.Column(db.Integer, nullable=False)
+    provincia = db.Column(db.String(30), nullable=False)
+    localidad = db.Column(db.String(30), nullable=False)
+    direccion = db.Column(db.String(60), nullable=False)
+    repartidor = db.relationship(
+        "Repartidor", backref="sucursal", cascade="all, delete-orphan"
+    )
+    transporte = db.relationship(
+        "Transporte", backref="sucursal", cascade="all, delete-orphan"
+    )
+    paquete = db.relationship(
+        "Paquete", backref="sucursal", cascade="all, delete-orphan"
+    )
+
+    def serialize(self):
+        return dict(
+            id=self.id,
+            numero=self.numero,
+            provincia=self.provincia,
+            localidad=self.localidad,
+            direccion=self.direccion,
+            repartidor=self.repartidor,
+            transporte=self.transporte,
+            paquete=self.paquete,
+        )
 
 
 class Transporte(db.Model):
     __tablename__ = "transporte"
     id = db.Column(db.Integer, primary_key=True)
-    numero = db.Column(db.Integer)
-    salida = db.Column(db.DateTime)
-    llegada = db.Column(db.DateTime)
-    id_sucursal = db.Column(db.Integer, db.ForeignKey("sucursal.id"))
+    numerotransporte = db.Column(db.Integer, nullable=False)
+    fechahorasalida = db.Column(db.DateTime)
+    fechahorallegada = db.Column(db.DateTime)
+    idsucursal = db.Column(db.Integer, db.ForeignKey("sucursal.id"))
+    paquete = db.relationship(
+        "Paquete", backref="transporte", cascade="all, delete-orphan"
+    )
+
+
+class Repartidor(db.Model):
+    __tablename__ = "repartidor"
+    id = db.Column(db.Integer, primary_key=True)
+    numero = db.Column(db.Integer, nullable=False)
+    nombre = db.Column(db.String(60), nullable=False)
+    dni = db.Column(db.String(8), nullable=False)
+    idsucursal = db.Column(db.Integer, db.ForeignKey("sucursal.id"))
+    paquete = db.relationship(
+        "Paquete", backref="repartidor", cascade="all, delete-orphan"
+    )
+
+
+class Paquete(db.Model):
+    __tablename__ = "paquete"
+    id = db.Column(db.Integer, primary_key=True)
+    numeroenvio = db.Column(db.Integer, nullable=False)
+    peso = db.Column(db.Integer)
+    nomdestinatario = db.Column(db.String(60), nullable=False)
+    dirdestinatario = db.Column(db.String(100), nullable=False)
+    entregado = db.Column(db.Boolean)
+    observaciones = db.Column(db.Text)
+    idsucursal = db.Column(db.Integer, db.ForeignKey("sucursal.id"))
+    idtransporte = db.Column(db.Integer, db.ForeignKey("transporte.id"))
+    idrepartidor = db.Column(db.Integer, db.ForeignKey("repartidor.id"))
